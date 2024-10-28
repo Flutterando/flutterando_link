7efda37bb0e1dcfa4dca1cc274a7b98f1f9b8fa1
Jacob Moura <jacobaraujo7@gmail.com>
2024-10-28 14:11:47 -0300

    added remote datasource

--- lib/app/data/datasources/remote_body_datasource.dart
+++ lib/app/data/datasources/remote_body_datasource.dart
@@ -1,9 +1,21 @@
+import 'package:uno/uno.dart';
+
 import 'body_datasource.dart';
 
+const _bodyJsonUrl = 'https://raw.githubusercontent.com/Flutterando/flutterando_link/refs/heads/main/assets/body.json';
+
 class RemoteBodyDatasource implements BodyDatasource {
+  final Uno uno;
+
+  RemoteBodyDatasource(this.uno);
+
   @override
-  Future<Map<String, dynamic>> getJsonBody() {
-    // TODO: implement getJsonBody
-    throw UnimplementedError();
+  Future<Map<String, dynamic>> getJsonBody() async {
+    final response = await uno.get(
+      _bodyJsonUrl,
+      responseType: ResponseType.json,
+    );
+
+    return response.data;
   }
 }
