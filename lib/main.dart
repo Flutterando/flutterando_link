287eb4dc9960a3095a0f649d8d05137ab26e78c7
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-10-31 08:16:45 -0300

    replace Container with AppWidget in main function to initialize the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'app/app_widget.dart';
 import 'app/injector.dart';
 
 void main() {
   setupInjection();
-  runApp(Container());
+  runApp(
+    const AppWidget(),
+  );
 }
