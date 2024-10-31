454d001d075db98b0913549fdb51908366ae05f1
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-10-31 08:16:40 -0300

    add LinkPage widget for displaying links in the Flutter application

---
+++ lib/app/pages/link_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LinkPage extends StatelessWidget {
+  const LinkPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pagina de Links Flutterando'),
+      ),
+      body: Container(),
+    );
+  }
+}
